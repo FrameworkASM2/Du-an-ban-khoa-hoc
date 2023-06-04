2f47ab5b4c66d5418bc80f19ca715d17aa42201e
lathitien <tienltph19794@fpt.edu.vn>
2023-06-05 03:47:21 +0700

    feat: CheckPermission refs: AAF-8

--- src/Middlewares/CheckPermission.js
+++ src/Middlewares/CheckPermission.js
@@ -11,12 +11,12 @@ export const checkPermission = async (req, res, next) => {
         const token = req.headers.authorization.split(" ")[1];
         Jwt.verify(token, "Ma hop le", async (error, payload) => {
             if (error) {
-                if (error.Name == "TokenExpiredError") {
+                if (error.name == "TokenExpiredError") {
                     return res.json({
                         message: "Token hết hạn"
                     })
                 }
-                if (error.Name == "JsonWebTokenError") {
+                if (error.name == "JsonWebTokenError") {
                     return res.json({
                         message: "Token không hợp lệ"
                     })
@@ -24,8 +24,9 @@ export const checkPermission = async (req, res, next) => {
             }
             const user = await User.findById(payload._id);
             console.log("user", user);
+
             if (user.role !== "admin") {
-                return res.status.json({
+                return res.status(400).json({
                     message: "Bạn không có quyền truy cập vào tài nguyên này"
                 })
             }
