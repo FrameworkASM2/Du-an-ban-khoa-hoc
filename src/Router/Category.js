ed8e506cc169d03a1ac85952dd67d24cdcff2aa4
lathitien <tienltph19794@fpt.edu.vn>
2023-06-05 16:20:31 +0700

    feat: Create router category refs: AAF-11

--- src/Router/Category.js
+++ src/Router/Category.js
@@ -1,7 +1,13 @@
 import express from "express";
 import { checkPermission } from "../middlewares/CheckPermission";
+import { create, get, getAll, remove, update } from "../Controllers/Category";
 
 const router = express.Router();
 
+router.get("/categories", getAll)
+router.get("/categories/:id", get)
+router.delete("/categories/:id", checkPermission, remove)
+router.post("/categories", checkPermission, create)
+router.put("/categories/:id", checkPermission, update)
 
 export default router;
