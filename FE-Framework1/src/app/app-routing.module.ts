3172bb1494224f3cb3d7a588b5217df9d3565b90
lathitien <tienltph19794@fpt.edu.vn>
2023-06-07 04:42:12 +0700

    feat: Create Add Course refs: AFF-15

--- FE-Framework1/src/app/app-routing.module.ts
+++ FE-Framework1/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AdminLayoutComponent } from './Layouts/admin-layout/admin-layout.compon
 import { DashboardComponent } from './Pages/Admin/dashboard/dashboard.component';
 import { CourseComponent } from './Components/course/course.component';
 import { AdminCategoryComponent } from './Pages/Admin/Admin-Category/admin-category/admin-category.component';
+import { AddCourseComponent } from './Pages/add-course/add-course.component';
 
 const routes: Routes = [
   {
@@ -14,8 +15,16 @@ const routes: Routes = [
       { path: "category", component: AdminCategoryComponent }
 
     ]
+  },
+  {
+    path: 'admin', component: AdminLayoutComponent, children: [
+      { path: "", redirectTo: "dashboard", pathMatch: "full" },
+      { path: "dashboard", component: DashboardComponent },
+      { path: "course", component: CourseComponent },
+      { path: "course/add", component: AddCourseComponent }
+    ]
   }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
