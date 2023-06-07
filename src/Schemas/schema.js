e28df4bf2747248074ca1acfac4d0de01204fe1b
Bich <hoangbich181203@gmail.com>
2023-06-07 23:47:41 +0700

    feat:  "AAF-7 <fix bug Schema>" refs: AAF-7

--- src/Schemas/schema.js
+++ src/Schemas/schema.js
@@ -1,55 +1,48 @@
 import joi from 'joi';
 
 export const productSchema = joi.object({
-  name: joi.string().required().messages({
+  Name: joi.string().required().messages({
     'string.empty': 'Vui lòng nhập tên sản phẩm',
     'any.required': 'Vui lòng nhập tên sản phẩm',
   }),
-  price: joi.number().required().messages({
+  Price: joi.number().required().messages({
     'number.base': 'Vui lòng nhập giá sản phẩm',
     'any.required': 'Vui lòng nhập giá sản phẩm',
   }),
-  description: joi.string().required().messages({
+  Description: joi.string().required().messages({
     'string.empty': 'Vui lòng nhập mô tả sản phẩm',
     'any.required': 'Vui lòng nhập mô tả sản phẩm',
   }),
 });
 
 export const categorySchema = joi.object({
-  name: joi.string().required().messages({
+  Name: joi.string().required().messages({
     'string.empty': 'Vui lòng nhập tên danh mục',
     'any.required': 'Vui lòng nhập tên danh mục',
   }),
 });
 
 export const signupSchema = joi.object({
-  name: joi.string().required().messages({
+  Name: joi.string().required().messages({
     'string.empty': 'Vui lòng nhập tên',
     'any.required': 'Vui lòng nhập tên',
   }),
-  email: joi.string().email().required().messages({
+  Email: joi.string().email().required().messages({
     'string.email': 'Vui lòng nhập địa chỉ email hợp lệ',
     'any.required': 'Vui lòng nhập địa chỉ email',
   }),
-  password: joi.string().min(6).required().messages({
+  Password: joi.string().min(6).required().messages({
     'string.min': 'Mật khẩu phải chứa ít nhất 6 ký tự',
     'any.required': 'Vui lòng nhập mật khẩu',
-  }),
-  confirmPassword: joi.string()
-    .valid(joi.ref('password'))
-    .required()
-    .messages({
-      'any.only': 'Mật khẩu xác nhận phải trùng khớp với mật khẩu',
-      'any.required': 'Vui lòng nhập mật khẩu xác nhận',
-    }),
+  })
 });
 
 export const signinSchema = joi.object({
-  email: joi.string().email().required().messages({
+  Email: joi.string().email().required().messages({
     'string.email': 'Vui lòng nhập địa chỉ email hợp lệ',
     'any.required': 'Vui lòng nhập địa chỉ email',
   }),
-  password: joi.string().min(6).required().messages({
+  Password: joi.string().min(6).required().messages({
     'string.min': 'Mật khẩu phải chứa ít nhất 6 ký tự',
     'any.required': 'Vui lòng nhập mật khẩu',
   }),
