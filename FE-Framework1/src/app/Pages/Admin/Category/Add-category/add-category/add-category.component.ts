4d683d47ea01e27a0e931edeffd1cac3d340ccf2
Thuyngan <lanthuyngan@gmail.com>
2023-06-07 14:52:37 +0700

    feat:create-add_category refs:AAF-26

--- FE-Framework1/src/app/Pages/Admin/Category/Add-category/add-category/add-category.component.ts
+++ FE-Framework1/src/app/Pages/Admin/Category/Add-category/add-category/add-category.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ICategory } from 'src/app/Interfaces/Category';
+import { CategoryService } from 'src/app/Services/Category/category-service.service';
 
 @Component({
   selector: 'app-add-category',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-category.component.css']
 })
 export class AddCategoryComponent {
+  categories: ICategory = {
+    Name: ""
+  }
+  constructor(private categoryServices: CategoryService) {
+
+  }
+  onHandleSubmit() {
+    this.categoryServices.addCategory(this.categories).subscribe(category => {
+      alert("Bạn đã thêm danh mục thành công")
+
+
+
+    })
+  }
 
 }
--- FE-Framework1/src/app/app-routing.module.ts
+++ FE-Framework1/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AdminLayoutComponent } from './Layouts/admin-layout/admin-layout.compon
 import { DashboardComponent } from './Pages/Admin/dashboard/dashboard.component';
 import { CourseComponent } from './Components/course/course.component';
 import { AdminCategoryComponent } from './Pages/Admin/Admin-Category/admin-category/admin-category.component';
+import { AddCategoryComponent } from './Pages/Admin/Category/Add-category/add-category/add-category.component';
 
 const routes: Routes = [
   {
@@ -11,7 +12,8 @@ const routes: Routes = [
       { path: "", redirectTo: "dashboard", pathMatch: "full" },
       { path: "dashboard", component: DashboardComponent },
       { path: "course", component: CourseComponent },
-      { path: "category", component: AdminCategoryComponent }
+      { path: "category", component: AdminCategoryComponent },
+      { path: "category/add", component: AddCategoryComponent }
 
     ]
   }
