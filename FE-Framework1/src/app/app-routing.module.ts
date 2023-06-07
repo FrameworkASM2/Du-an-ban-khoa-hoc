bbabe07e0b1481dd2f619b86ab8893e4224ae0eb
lathitien <tienltph19794@fpt.edu.vn>
2023-06-07 18:47:26 +0700

    feat: Update

--- FE-Framework1/src/app/app-routing.module.ts
+++ FE-Framework1/src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
 import { AdminLayoutComponent } from './Layouts/admin-layout/admin-layout.component';
 import { DashboardComponent } from './Pages/Admin/dashboard/dashboard.component';
-import { CourseComponent } from './Components/course/course.component';
-import { AdminCategoryComponent } from './Pages/Admin/Admin-Category/admin-category/admin-category.component';
+import { CourseComponent } from './Pages/Admin/Course/course/course.component';
+import { AdminCategoryComponent } from './Pages/Admin/Category/admin-category/admin-category.component';
 import { AddCategoryComponent } from "./Pages/Admin/Category/Add-category/add-category/add-category.component";
-import { AddCourseComponent } from './Pages/add-course/add-course.component';
-import { UpdateCourseComponent } from './Pages/update-course/update-course.component';
+import { AddCourseComponent } from './Pages/Admin/Course/add-course/add-course.component';
+import { UpdateCourseComponent } from './Pages/Admin/Course/update-course/update-course.component';
 import { EditCategoryComponent } from "./Pages/Admin/Category/edit-category/edit-category.component";
 
 
@@ -15,16 +15,17 @@ const routes: Routes = [
     path: 'admin', component: AdminLayoutComponent, children: [
       { path: "", redirectTo: "dashboard", pathMatch: "full" },
       { path: "dashboard", component: DashboardComponent },
-//       === Course ===
+      //       === Course ===
       { path: "course", component: CourseComponent },
       { path: "course/add", component: AddCourseComponent },
       { path: "course/:id/edit", component: UpdateCourseComponent },
-      
-      //=== Category ===
+
+      //      === Category ===
       { path: "category", component: AdminCategoryComponent },
       { path: "category/add", component: AddCategoryComponent },
-      { path: "category/edit/:id", component:EditCategoryComponent }
-  },
+      { path: "category/edit/:id", component: EditCategoryComponent }
+    ]
+  }
 ]
 
 @NgModule({
--- FE-Framework1/src/app/app.module.ts
+++ FE-Framework1/src/app/app.module.ts
@@ -2,23 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './Pages/Admin/dashboard/dashboard.component';
-import { AdminCourseComponent } from './Pages/Admin/admin-course/admin-course.component';
 import { AdminLayoutComponent } from './Layouts/admin-layout/admin-layout.component';
 import { BaseLayoutComponent } from './Layouts/base-layout/base-layout.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
-import { CourseComponent } from './Components/course/course.component';
-import { AdminCategoryComponent } from './Pages/Admin/Admin-Category/admin-category/admin-category.component';
+import { CourseComponent } from './Pages/Admin/Course/course/course.component';
+import { AdminCategoryComponent } from './Pages/Admin/Category/admin-category/admin-category.component';
 import { AddCategoryComponent } from './Pages/Admin/Category/Add-category/add-category/add-category.component'
-import { AddCourseComponent } from './Pages/add-course/add-course.component';
-import { UpdateCourseComponent } from './Pages/update-course/update-course.component';
+import { AddCourseComponent } from './Pages/Admin/Course/add-course/add-course.component';
+import { UpdateCourseComponent } from './Pages/Admin/Course/update-course/update-course.component';
 import { EditCategoryComponent } from './Pages/Admin/Category/edit-category/edit-category.component'
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    AdminCourseComponent,
     AdminLayoutComponent,
     BaseLayoutComponent,
     CourseComponent,
