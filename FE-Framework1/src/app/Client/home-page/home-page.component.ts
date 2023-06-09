9756ff4a0d510ace84888390706e459869bd93b3
Hoang Thi Ngoc Bich <hoangbich181203@gmail.com>
2023-06-09 18:46:24 +0700

    feat: update home page component refs: AAF-45

--- FE-Framework1/src/app/Client/home-page/home-page.component.ts
+++ FE-Framework1/src/app/Client/home-page/home-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ICategory } from 'src/app/Interfaces/Category';
+import { ICourse } from 'src/app/Interfaces/Course';
+import { CategoryService } from 'src/app/Services/Category/category-service.service';
+import { CourseService } from 'src/app/Services/Course/course.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  course: ICourse[] = [];
+
+  title = ""
+  constructor(private courservice: CourseService) {
+    this.courservice.getAllCourse(10).subscribe(data => {
+      this.course = data
+    },
+      error => {
+        console.log(error);
+      })
+  }
 
 }
