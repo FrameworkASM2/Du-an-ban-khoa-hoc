21049a37818bcc82d9966740ebe3d7ea46ff771c
lathitien <tienltph19794@fpt.edu.vn>
2023-06-13 00:52:00 +0700

    feat: Search filter course refs: AAF-38

--- FE-Framework1/src/app/Pages/Course/list-course/list-course.component.ts
+++ FE-Framework1/src/app/Pages/Course/list-course/list-course.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ICategory } from 'src/app/Interfaces/Category';
 import { ICourse } from 'src/app/Interfaces/Course';
+import { CategoryService } from 'src/app/Services/Category/category-service.service';
 import { CourseService } from 'src/app/Services/Course/course.service';
 
 @Component({
@@ -9,14 +12,43 @@ import { CourseService } from 'src/app/Services/Course/course.service';
 })
 export class ListCourseComponent {
   courses: ICourse[] = [];
-  constructor(private courseService: CourseService) {
+  categories: ICategory[] = []
+  // catId: any;
+  constructor(
+    private courseService: CourseService,
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+  ) {
     this.courseService.getAllCourse(4).subscribe((course) => {
       this.courses = course
     },
       error => {
         console.log(error);
 
-      })
+      }),
+      this.categoryService.getCategory().subscribe(data => {
+        this.categories = data
+      },
+        error => {
+          console.log(error);
+
+        })
+
+    // this.route.paramMap.subscribe(param => {
+    //   const catId = Number(param.get('_id'));
+    //   // this.categoryService.getcategoryById(id).subscribe(category => {
+    //   //   this.category = category;
+    //   this.getProductCatId(catId)
+
+    // }, error => console.log(error.message))
   }
+
+  // getProductCatId(id: any) {
+  //   this.courseService.getAllCourseByCat(id).subscribe((product) => {
+  //     this.courses = product
+  //   })
+  // }
+
+  searchValue = ""
 
 }
